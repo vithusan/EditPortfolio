bd15eb2b1b063cf892f6f6bfd0df41d8c5380109
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2020-01-22 18:04:32 -0500

    changed contact icons

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -4,20 +4,15 @@ class Contact extends Component {
     render() {
         return (
             <div className='contactDiv'>
-                <div className='emailId'>
-                    <a href="mailto:vithushan1011@hotmail.com" target="_top" >VITHUSHAN1011@HOTMAIL.COM</a>
-                </div>
-                <div className='listOfContactIcons'>
-                    <a href="https://github.com/vithusan">
-                        <img src='/img/github.png' alt="socialMedia" className='contactIcons1' />
-                    </a>
-                    <a href="https://www.linkedin.com/in/vithusan-kanageswaran/">
-                        <img src='/img/linkedin.png' alt="socialMedia" className='contactIcons' />
-                    </a>
-                    <a href="https://www.instagram.com/vk_kanageswaran/">
-                        <img src='/img/instagram.png' alt="socialMedia" className='contactIcons' />
-                    </a>
-                </div>
+                <a href="https://github.com/vithusan" target="_blank">
+                    <img src='/img/githubIcon.png' alt="socialMedia" className='contactIcons' />
+                </a>
+                <a href="mailto:vithushan1011@hotmail.com" target="_top">
+                    <img src='/img/emailIcon.png' alt="socialMedia" className='contactIcons' />
+                </a>
+                <a href="https://www.linkedin.com/in/vithusan-kanageswaran/" target="_blank">
+                    <img src='/img/linkedinIcon.png' alt="socialMedia" className='contactIcons' />
+                </a>
             </div>
         );
     }
